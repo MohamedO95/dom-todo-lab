4a0c373f5821f159a0195a3119bce14a93395503
MohamedO95 <mohamedkao@hotmail.com>
2022-09-01 03:02:23 -0400

    complete exercise

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const todoInput = document.getElementById('todo-input')
+const submitBtn = document.getElementById('submit-button')
+const todoList = document.getElementById('todo-list')
+const resetBtn = document.getElementById('reset-button')
+
+// --==Event Listeners==--
+
+// Functionality to add to to-dos
+submitBtn.addEventListener('click', function(event) {
+  if(todoInput.value === "") return
+  const newTodo = document.createElement('li')
+  newTodo.textContent = todoInput.value
+  todoList.appendChild(newTodo)
+  todoInput.value = ''
+})
+
+// Functionality to remove individual to-dos
+todoList.addEventListener('click', function(event) {
+  const parentNode = event.target.parentNode
+  parentNode.removeChild(event.target)
+})
+
+// Functionality for Reset button to remove all to-dos
+resetBtn.addEventListener('click', function(event) {
+  todoList.innerHTML = ''
+  todoInput.value = ''
+})
+
